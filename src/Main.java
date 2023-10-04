e1ca21f946df5a9495d11329023257c8b44256c4
kstubhieeee <144565193+kstubhieeee@users.noreply.github.com>
2023-10-04 15:22:00 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+package main;
+
+import java.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Kaustubh!");
+
+        JFrame frame = new JFramw();
+        frame.setMinimumSize(new Dimension(1000,1000));
+        frame.setLocationRelativeto(null);
     }
 }
