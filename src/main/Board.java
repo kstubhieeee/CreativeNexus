750e08a99e25608ac4eaabe795c258b6aa58c4bf
kstubhieeee <banekaustubh27@gmail.com>
2023-10-05 02:02:08 +0530

    basic

--- src/main/Board.java
+++ src/main/Board.java
@@ -15,5 +15,18 @@ public class Board extends JPanel{
         this.setBackground(Color.green);
 
     }
+
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D g2d= (Graphics2D)  g;
+
+        for(int r=0;r<rows;r++)
+            for (int c=0;c<cols;c++)
+            {
+                g2d.setColor((c+r)%2==0? new Color(234, 183, 130):new Color(231, 144, 63));
+                g2d.fillRect(c*tilesize, r*tilesize,tilesize,tilesize);
+            }
+    }
+
 }
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
+        frame.getContentPane().setBackground(Color.black);
+        frame.setLayout(new GridBagLayout());
         frame.setMinimumSize(new Dimension(1000,1000));
         frame.setLocationRelativeTo(null);
 
