b25c417019fe470585a3d4791c39b7a2400c9b97
kstubhieeee <144565193+kstubhieeee@users.noreply.github.com>
2023-10-05 12:02:23 +0530

    Update Piece.java

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,4 +1,34 @@
 package pieces;
 
+import java.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class Piece {
+
+  public int col, row;
+  public int xPos, yPos;
+
+  public boolean isWhite;
+  public String name;
+  public int value;
+
+  BufferedImage sheet;
+  {
+    try{
+      sheet = ImageIO.read(ClassLoader.getSystemResourceAsStream("pieces.png"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+Image sprite;
+
+  Board board
+  
+public void Piece(Board board) {
+  this.board=board;
+
+}
 }
